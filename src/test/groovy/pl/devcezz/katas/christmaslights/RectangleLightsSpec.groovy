a85a51c08cbf1248ca00c83f3a684d1084b6fa17
Cezary Sanecki <devcezz@gmail.com>
2022-08-08 22:54:48 +0200

    Tested turning on lights on specified area

--- src/test/groovy/pl/devcezz/katas/christmaslights/RectangleLightsSpec.groovy
+++ src/test/groovy/pl/devcezz/katas/christmaslights/RectangleLightsSpec.groovy
@@ -19,4 +19,22 @@ class RectangleLightsSpec extends Specification {
             2    || 4
             15   || 225
     }
+
+    def "should turn lights on specified area"() {
+        given: "Prepare rectangle of lights."
+            RectangleLights lights = new RectangleLights(10)
+
+        when: "Turn on lights on specified area."
+            lights.turnOn(Point.of(firstX, firstY), Point.of(secondX, secondY))
+
+        then: "Lights are turned on on specified area"
+            lights.countTurnedOnLights() == expectedTurnedOnLights
+
+        where:
+            firstX | firstY | secondX | secondY || expectedTurnedOnLights
+            0      | 0      | 3       | 4       || 20
+            2      | 3      | 4       | 5       || 9
+            1      | 1      | 9       | 9       || 81
+            4      | 4      | 7       | 9       || 24
+    }
 }
