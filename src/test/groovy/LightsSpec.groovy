f9858076c01bebd95939347d4168d238c60b62b5
Cezary Sanecki <devcezz@gmail.com>
2022-08-08 18:02:39 +0200

    Added first test to check that all lights are turned off on start

---
+++ src/test/groovy/LightsSpec.groovy
@@ -0,0 +1,18 @@
+import spock.lang.Specification
+
+class LightsSpec extends Specification {
+
+    def "should all lights be turned off on start"() {
+        when: "Prepare rectangle of lights."
+            Lights Lights = new Lights(firstSideOfRectagle, secondSideOfRectagle)
+
+        then: "All lights are turned off."
+            Lights.getTurnedOffLigths().size() == turnedOffLights
+
+        where:
+            firstSideOfRectagle | secondSideOfRectagle || turnedOffLights
+            10                  | 10                   || 100
+            2                   | 3                    || 3
+            15                  | 12                   || 180
+    }
+}
