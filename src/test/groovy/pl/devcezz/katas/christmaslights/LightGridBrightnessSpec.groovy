4fd529e8d0739a131c8710372d2f9c61ebf25347
Cezary Sanecki <devcezz@gmail.com>
2022-08-09 12:32:52 +0200

    Added test for darking lights

--- src/test/groovy/pl/devcezz/katas/christmaslights/LightGridBrightnessSpec.groovy
+++ src/test/groovy/pl/devcezz/katas/christmaslights/LightGridBrightnessSpec.groovy
@@ -29,4 +29,17 @@ class LightGridBrightnessSpec extends Specification {
             1      | 1      | 9       | 9       || 81
             4      | 4      | 7       | 9       || 24
     }
+
+    def "should darken lights on specified area"() {
+        given: "Prepare rectangle of lights."
+            LightGrid lights = new LightGrid(10)
+        and: "Light lights on specified area."
+            lights.lighten(Point.of(0, 0), Point.of(9, 9))
+
+        when: "Turn off lights on specified area."
+            lights.darken(Point.of(1, 1), Point.of(8, 8))
+
+        then: "Some lights are darkened by 1."
+            lights.countBrightness() == 36
+    }
 }
