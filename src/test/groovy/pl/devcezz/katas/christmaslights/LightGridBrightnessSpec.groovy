abfbb5f9530a7151717a36a6399ef0c6754a860f
Cezary Sanecki <devcezz@gmail.com>
2022-08-09 12:19:39 +0200

    Added test for lighten lights on specified area

--- src/test/groovy/pl/devcezz/katas/christmaslights/LightGridBrightnessSpec.groovy
+++ src/test/groovy/pl/devcezz/katas/christmaslights/LightGridBrightnessSpec.groovy
@@ -11,4 +11,22 @@ class LightGridBrightnessSpec extends Specification {
         then: "Brightness is zero."
             lights.countBrightness() == 0
     }
+
+    def "should lighten lights on specified area"() {
+        given: "Prepare rectangle of lights."
+            LightGrid lights = new LightGrid(10)
+
+        when: "Lighten lights on specified area."
+            lights.lighten(Point.of(firstX, firstY), Point.of(secondX, secondY))
+
+        then: "All lights are brightened by 1."
+            lights.countBrightness() == expectedBrightness
+
+        where:
+            firstX | firstY | secondX | secondY || expectedBrightness
+            0      | 0      | 3       | 4       || 20
+            2      | 3      | 4       | 5       || 9
+            1      | 1      | 9       | 9       || 81
+            4      | 4      | 7       | 9       || 24
+    }
 }
