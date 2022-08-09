d8409b1ebc53180114a877b41623e5697c52396e
Cezary Sanecki <devcezz@gmail.com>
2022-08-09 11:31:45 +0200

    Refactored method

--- src/main/java/pl/devcezz/katas/christmaslights/RectangleLights.java
+++ src/main/java/pl/devcezz/katas/christmaslights/RectangleLights.java
@@ -1,6 +1,7 @@
 package pl.devcezz.katas.christmaslights;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 class RectangleLights {
@@ -35,60 +36,54 @@ class RectangleLights {
         if (leftBottomCorner == null || rightTopCorner == null) {
             throw new IllegalArgumentException("points cannot be null");
         }
-        if (leftBottomCorner.x() > rightTopCorner.x()) {
-            throw new IllegalArgumentException("left corner cannot be greater then right corner");
-        }
-        if (leftBottomCorner.y() > rightTopCorner.y()) {
-            throw new IllegalArgumentException("bottom corner cannot be greater then top corner");
-        }
 
-        int horizontalLengthOfArea = rightTopCorner.x() - leftBottomCorner.x();
-        int verticalLengthOfArea = rightTopCorner.y() - leftBottomCorner.y();
+        Area area = prepareArea(leftBottomCorner, rightTopCorner);
 
-        int startingHorizontalPosition = leftBottomCorner.x();
-        int endingHorizontalPosition = leftBottomCorner.x() + horizontalLengthOfArea;
-
-        int startingVerticalPosition = leftBottomCorner.y();
-        int endingVerticalPosition = leftBottomCorner.y() + verticalLengthOfArea;
-
-        for (int i = startingHorizontalPosition; i <= endingHorizontalPosition; i++) {
-            for (int j = startingVerticalPosition; j <= endingVerticalPosition; j++) {
-                Light light = area[i][j];
-                light.turnOn();
-            }
-        }
+        makeActionOnEveryLightInArea(area, Light::turnOn);
     }
 
     void turnOff(Point leftBottomCorner, Point rightTopCorner) {
         if (leftBottomCorner == null || rightTopCorner == null) {
             throw new IllegalArgumentException("points cannot be null");
         }
-        if (leftBottomCorner.x() > rightTopCorner.x()) {
-            throw new IllegalArgumentException("left corner cannot be greater then right corner");
-        }
-        if (leftBottomCorner.y() > rightTopCorner.y()) {
-            throw new IllegalArgumentException("bottom corner cannot be greater then top corner");
-        }
 
-        int horizontalLengthOfArea = rightTopCorner.x() - leftBottomCorner.x();
-        int verticalLengthOfArea = rightTopCorner.y() - leftBottomCorner.y();
+        Area area = prepareArea(leftBottomCorner, rightTopCorner);
 
-        int startingHorizontalPosition = leftBottomCorner.x();
-        int endingHorizontalPosition = leftBottomCorner.x() + horizontalLengthOfArea;
-
-        int startingVerticalPosition = leftBottomCorner.y();
-        int endingVerticalPosition = leftBottomCorner.y() + verticalLengthOfArea;
-
-        for (int i = startingHorizontalPosition; i <= endingHorizontalPosition; i++) {
-            for (int j = startingVerticalPosition; j <= endingVerticalPosition; j++) {
-                Light light = area[i][j];
-                light.turnOff();
-            }
-        }
+        makeActionOnEveryLightInArea(area, Light::turnOff);
     }
 
     private Stream<Light> streamOfLights() {
         return Arrays.stream(area).flatMap(Arrays::stream);
+    }
+
+    private Area prepareArea(Point leftBottomCorner, Point rightTopCorner) {
+        int horizontalLengthOfArea = rightTopCorner.x() - leftBottomCorner.x();
+        int verticalLengthOfArea = rightTopCorner.y() - leftBottomCorner.y();
+
+        return new Area(
+                leftBottomCorner.x(), leftBottomCorner.x() + horizontalLengthOfArea,
+                leftBottomCorner.y(), leftBottomCorner.y() + verticalLengthOfArea);
+    }
+
+    private void makeActionOnEveryLightInArea(Area area, Consumer<Light> action) {
+        for (int i = area.startingHorizontalPosition(); i <= area.endingHorizontalPosition(); i++) {
+            for (int j = area.startingVerticalPosition(); j <= area.endingVerticalPosition(); j++) {
+                Light light = this.area[i][j];
+                action.accept(light);
+            }
+        }
+    }
+
+    private record Area(int startingHorizontalPosition, int endingHorizontalPosition,
+                        int startingVerticalPosition, int endingVerticalPosition) {
+        Area {
+            if (startingHorizontalPosition > endingHorizontalPosition) {
+                throw new IllegalArgumentException("left corner cannot be greater then right corner");
+            }
+            if (startingVerticalPosition > endingVerticalPosition) {
+                throw new IllegalArgumentException("bottom corner cannot be greater then top corner");
+            }
+        }
     }
 }
 
