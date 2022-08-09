dd53062304a94e44ecee353168f6cd7c0ed92b9b
Cezary Sanecki <devcezz@gmail.com>
2022-08-09 12:16:39 +0200

    Added test for brightness at start of light grid

---
+++ src/test/groovy/pl/devcezz/katas/christmaslights/LightGridBrightnessSpec.groovy
@@ -0,0 +1,14 @@
+package pl.devcezz.katas.christmaslights
+
+import spock.lang.Specification
+
+class LightGridBrightnessSpec extends Specification {
+
+    def "should brightness be zero when instantiate light gird"() {
+        when: "Prepare rectangle of lights."
+            LightGrid lights = new LightGrid(10)
+
+        then: "Brightness is zero."
+            lights.countBrithness() == 0
+    }
+}
